f360408d9412899723898aca7702b4ebf4fa1529
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-28 00:57:51 +0100

    display realtime data

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -17,8 +17,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -43,25 +46,22 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.data);
 
         FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://fir-demo-88800-default-rtdb.europe-west1.firebasedatabase.app/");
 
-        logout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this,"Logged Out!",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this,StartActivity.class));
-                finish();
-            }
+
+        logout.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(MainActivity.this,"Logged Out!",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this,StartActivity.class));
+            finish();
         });
 
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt_name = edit.getText().toString();
-                if(isEmpty(txt_name))
-                    Toast.makeText(MainActivity.this,"No Name Entred!",Toast.LENGTH_SHORT).show();
-                else{
-                    /* Write a message to the realtime database*/
+        add.setOnClickListener(v -> {
+            String txt_name = edit.getText().toString();
+            if(isEmpty(txt_name))
+                Toast.makeText(MainActivity.this,"No Name Entred!",Toast.LENGTH_SHORT).show();
+            else{
+                /* Write a message to the realtime database*/
 //                    Map<String,String> userMap = new HashMap<>();
 //                    userMap.put("name",txt_name);
 //                    mFirestore.collection("Users").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
@@ -71,17 +71,34 @@ public class MainActivity extends AppCompatActivity {
 //                        }
 //                    });
 
-                    /* Write a message to the firestore database*/
-                    FirebaseDatabase database = FirebaseDatabase.getInstance("https://fir-demo-88800-default-rtdb.europe-west1.firebasedatabase.app/");
-                    DatabaseReference myRef = database.getReference();
-                    myRef.child("ProgrammingKnowledge").push().child("Name").setValue(txt_name);
+                /* Write a message to the firestore database*/
+                DatabaseReference myRef = database.getReference();
+                myRef.child("Languages").child("Name").setValue(txt_name);
 
-                    Toast.makeText(MainActivity.this,"date sended!",Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(MainActivity.this,"date sended!",Toast.LENGTH_SHORT).show();
             }
         });
+
         ArrayList<String> list = new ArrayList<>();
         ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
         listView.setAdapter(adapter);
+
+        DatabaseReference reference = database.getReference().child("Languages");
+
+        reference.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot Snapshot : snapshot.getChildren()) {
+                    list.add(Snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
