b03658915a705c1be244eff607c9676cf26ae3c2
tweeel <houssembababendermel@gmail.com>
2021-11-28 17:06:12 +0100

    add onstart function that we don't to log in each time we start the app

--- app/src/main/java/com/example/firebasedemo/StartActivity.java
+++ app/src/main/java/com/example/firebasedemo/StartActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     private Button register,login;
@@ -34,5 +37,14 @@ public class StartActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            startActivity(new Intent(StartActivity.this,MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
     }
 }
